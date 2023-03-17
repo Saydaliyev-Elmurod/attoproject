d92b318fc78d6e41102415fadca4b92224c8dc98
Elmurod2203 <saydaliyevelmurod01@gmail.com>
2023-03-17 22:45:49 +0500

    project atto      finished project with jdbs yet dont add spring and hibernate

---
+++ src/main/java/dto/Profile.java
@@ -0,0 +1,26 @@
+package dto;
+
+import enums.Role;
+import enums.Status;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Profile {
+    private Integer id;
+    private String name;
+    private String surname;
+    private String phone;
+    private String password;
+    private LocalDateTime localDateTime;
+    private Status status;
+    private Role role;
+
+
+}
